88d07f347f2f328579f08df1dbc9c44436f0204f
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-12-01 21:51:17 +0200

    Update MapCollision.h

--- Source Code/Header Files/MapCollision.h
+++ Source Code/Header Files/MapCollision.h
@@ -1,1 +1,7 @@
+#pragma once
+#include <array>
 
+#include "Global.h"
+
+
+bool mapCollision(bool iCollectPellets, bool iUseDdoor, short x_pos, short y_pos, std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH>& Map);
