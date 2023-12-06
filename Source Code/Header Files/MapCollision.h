be52cffb020171e309ae03f797addf2eca9c884d
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-12-06 20:42:54 +0200

    Delete Source Code directory

--- Source Code/Header Files/MapCollision.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <array>
-
-#include "Global.h"
-
-
-bool mapCollision(bool iCollectPellets, bool iUseDdoor, short x_pos, short y_pos, std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH>& Map);
